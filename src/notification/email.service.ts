d104337c50fbad76f277487aec6675439de85e5a
Anja Lečnik <108223734+anjalecnik@users.noreply.github.com>
2025-11-30 17:07:12 +0100

    Changes:
    - Added resend for sending emails

--- src/notification/email.service.ts
+++ src/notification/email.service.ts
@@ -1,11 +1,27 @@
 import { Injectable, Logger } from '@nestjs/common';
+import { Resend } from 'resend';
 
 @Injectable()
 export class EmailService {
   private readonly logger = new Logger(EmailService.name);
+  private readonly resend = new Resend(process.env.RESEND_API_KEY);
 
   async sendEmail(to: string, subject: string, body: string): Promise<void> {
-    // TODO: implementacija realnega emaila (SMTP, SendGrid, ...)
-    this.logger.log(`Sending EMAIL to ${to} | subject: ${subject} | body: ${body}`);
+    try {
+      await this.resend.emails.send({
+        from: process.env.RESEND_FROM ?? 'noreply@example.com',
+        to,
+        subject,
+        html: body,
+      });
+
+      this.logger.log(`Email SENT to ${to} (${subject})`);
+    } catch (error: any) {
+      this.logger.error(
+        `Failed to send email to ${to}: ${error.message}`,
+        error,
+      );
+      throw error;
+    }
   }
 }
